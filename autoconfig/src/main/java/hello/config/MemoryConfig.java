83f2b690070d2d81a26560a70569ca36289afb79
wisehero10 <godjw96@gmail.com>
2023-03-28 12:31:34 +0900

    @Conditional 알아보기

--- autoconfig/src/main/java/hello/config/MemoryConfig.java
+++ autoconfig/src/main/java/hello/config/MemoryConfig.java
@@ -1,5 +1,6 @@
 package hello.config;
 
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -7,6 +8,9 @@ import memory.MemoryController;
 import memory.MemoryFinder;
 
 @Configuration
+// 이 클래스 안의 matches 결과가 True면 아래 설정정보가 추가되고 아니면 추가되지 않는다.
+// @Conditional(MemoryCondition.class)
+@ConditionalOnProperty(name = "memory", havingValue = "on")
 public class MemoryConfig {
 
 	@Bean
